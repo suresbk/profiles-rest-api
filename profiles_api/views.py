0c423072d5fec130acd16cf143be7a25dce10bc1
suresbk <skmessaih1@gmail.com>
2023-12-17 12:49:11 +0530

    HTTP methods added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 
 # Create your views here.
 class HelloApiView(APIView):
-
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         an_apiview = [
@@ -17,4 +19,26 @@ class HelloApiView(APIView):
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
     
 
-    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message': message})
+        
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
+
